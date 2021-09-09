cb9566ce6b75ab97339cb6e97d4c852768c57ce8
Neil Dhir <ndhir@turing.ac.uk>
2021-09-09 18:32:32 +0100

    Removed assign_blanket function

--- src/utils/postprocess.py
+++ src/utils/postprocess.py
@@ -16,46 +16,6 @@ def get_results(arm_played, rewards, mu):
     return results
 
 
-def assign_blanket(M: StructuralCausalModel, blanket: dict, best_intervention: dict, target_var_only: str,) -> dict:
-
-    assert isinstance(best_intervention, dict)
-
-    # Assign target value
-    causal_effect = M.query(outcome=(target_var_only,), intervention=best_intervention)
-    # Greedily pick the outcome value with the highest probability under the SCM model
-    # TODO: this is almost surely not correct, it should be the expected value OR more likely just the value of the SEM executed for the samples values of U plus the value of intervention. We probably also need to use the same samples of U throughout the whole process.
-    # TODO: this has the highest causal order so we should move it to the end of the recursion below.
-    blanket[target_var_only] = max(causal_effect, key=causal_effect.get)[0]
-
-    if best_intervention:
-        # Assign best intervention
-        for (key, value) in best_intervention.items():
-            blanket[key] = value
-
-        #  Assign un-assigned variables IF they are of higher causal order than the intervention
-        unassigned_vars = [key for key in blanket.keys() if blanket[key] == None]
-        #  We could compute this in the main function but we don't since the order could change per time-slice
-        causal_order = {var: M.G.causal_order().index(var) for var in blanket.keys() if var != target_var_only}
-        if len(unassigned_vars) > 0:
-            if len(best_intervention) == 1:
-                for key in best_intervention.keys():
-                    interv_idx = causal_order[key]
-            else:
-                raise NotImplementedError("The optimal intevention is multivariate. Have not thought about that yet.")
-
-            #  Check if the un-assigned nodes are of higher causal order
-            for var in unassigned_vars:
-                if causal_order[var] > interv_idx:
-                    pass
-        else:
-            return blanket
-
-        # XXX: note that nodes which are not in {outcome_var, interventions_vars} are _not_ assigned a value (currently). It is at present not clear what we should do with these variables. We could perhaps draw a sample from them and then use that.
-    else:
-        # This is option exist for rare situations where the empty set (i.e. best_intervention = {}) is the best intervention.
-        return blanket
-
-
 def implement_intervention(causal_order, F, mu1, best_intervention):
     assert isinstance(best_intervention, dict)
     assert (
@@ -69,7 +29,7 @@ def implement_intervention(causal_order, F, mu1, best_intervention):
     # This will eventually be our assigned time-slice.
     assigned = {key: bernoulli.rvs(success_prob) for (key, success_prob) in mu1.items()}
     for V_i in causal_order:
-        #  We do not assign acausal nodes
+        #  We do not assig acausal nodes
         if causal_order_idx[V_i] < causal_order_idx[V_interv]:
             assigned[V_i] = None
         else:
