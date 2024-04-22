e450afdda190b970a0b575145fde8057317ce466
Neil <ndhir@turing.ac.uk>
2024-04-22 12:44:29 -0700

    Update version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="ccb", version="1.0", packages=find_packages(), author="Neil Dhir", author_email="ndhir@turing.ac.uk")
+setup(name="ccb", version="0.1.0", packages=find_packages(), author="Neil Dhir", author_email="ndhir@turing.ac.uk")
